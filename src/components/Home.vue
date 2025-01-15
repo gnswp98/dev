36b1824da4421b1a4782a02ff747197bff1dd541
gnswp98 <chlwognschlw@gmail.com>
2025-01-15 23:37:57 +0900

    로그인 페이지 및 메인화면 배너 추가

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,125 @@
 <template>
-    <div>
-        <img src="@/assets/images/gov24_img.jpg">
+        <div>
+        <img id="govLogo" src="@/assets/images/gov24_img.jpg">
         <h1>정부24 안내 페이지</h1>
         <p>로그인, 민원 신청, 고객센터 등 필요한 정보를 모아둔 사이트입니다!</p>
     </div>
-</template>
 
-<style>
-</style>
+    <div id="banner" class="banner">
+      <div class="banner-image" :style="{ backgroundImage: `url(${photos[currentIndex]})` }"></div>
+      <div class="controls">
+        <button @click="previousPhoto">&lt;</button>
+        <button @click="toggleSlideshow">{{ isPlaying ? '| |' : '▷' }}</button>    
+        <button @click="nextPhoto">&gt;</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted, onUnmounted } from "vue";
+  
+  export default {
+    name: "PhotoBanner",
+    setup() {
+      // 경로 한 번에 몰아서 넣어둠
+      const photos = ref([
+        require("@/assets/images/20250110_main.png"), 
+        require("@/assets/images/MB_20240930_01.png"),
+      ]);
+      const currentIndex = ref(0);
+      const isPlaying = ref(true);
+      let interval = null;
+  
+      const startSlideshow = () => {
+        if (!interval) {
+          interval = setInterval(() => {
+            nextPhoto();
+          }, 3000);
+        }
+      };
+  
+      const stopSlideshow = () => {
+        clearInterval(interval);
+        interval = null;
+      };
+  
+      const toggleSlideshow = () => {
+        if (isPlaying.value) {
+          stopSlideshow();
+        } else {
+          startSlideshow();
+        }
+        isPlaying.value = !isPlaying.value;
+      };
+  
+      const nextPhoto = () => {
+        currentIndex.value = (currentIndex.value + 1) % photos.value.length;
+      };
+  
+      const previousPhoto = () => {
+        currentIndex.value =
+          (currentIndex.value - 1 + photos.value.length) % photos.value.length;
+      };
+  
+      onMounted(() => {
+        startSlideshow();
+      });
+  
+      onUnmounted(() => {
+        stopSlideshow();
+      });
+  
+      return {
+        photos,
+        currentIndex,
+        isPlaying,
+        toggleSlideshow,
+        nextPhoto,
+        previousPhoto,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .banner {
+    width: 1000px;
+    height: 400px;
+    
+    position: relative;
+    overflow: hidden;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+  }
+  
+  .banner-image {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    transition: background-image 0.5s ease-in-out;
+  }
+  
+  .controls {
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    display: flex;
+    gap: 10px;
+  }
+  
+  button {
+    padding: 10px 15px;
+    border: none;
+    background: #1b5dc1;
+    color: #fff;
+    font-size: 14px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+  
+  button:hover {
+    background-color: #113069;
+  }
+  </style>
+  
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,91 +1,131 @@
 <template>
-  <div class="inner-wrap">
-    <div class="visual-box">
-      <div class="visual-wrap swiper-container">
-        <div class="swiper-wrapper">
-          <div
-            class="swiper-slide"
-            v-for="(image, index) in images"
-            :key="index"
-          >
-            <a :href="image.link" target="_blank" title="새창열림">
-              <img :src="image.src" :alt="image.alt" />
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="swiper-navi">
-        <div class="swiper-pagination">
-          <span>{{ currentSlide }}</span> / <span>{{ totalSlides }}</span>
-        </div>
-        <button class="swiper-button-prev" @click="prevSlide">이전 슬라이드</button>
-        <button class="swiper-button-next" @click="nextSlide">다음 슬라이드</button>
-      </div>
-    </div>
+<div class="userinfo-box">
 
-    <div class="search-box">
-      <div class="tit">자주찾는 검색</div>
-      <div class="pop-search">
-        <ul class="keyword-list">
-          <li v-for="(keyword, index) in keywords" :key="index">
-            <a href="javascript:;" @click="searchKeyword(keyword)">
-              {{ index + 1 }}. {{ keyword }}
-            </a>
-          </li>
-        </ul>
-      </div>
-      <form @submit.prevent="search">
-        <input
-          type="text"
-          v-model="searchQuery"
-          placeholder="찾으시는 서비스를 입력하세요"
-          title="검색어 입력"
-        />
-        <button type="submit">검색</button>
-      </form>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+<div class="login-wrap">
+  <div class="login-info">
+    <h3 class="tit">내게 필요한 민원/생활/혜택<br>정보를 확인하세요.</h3>
+    <a href="/nlogin?Mcode=10003&amp;regType=ctab" class="btn-login">로그인</a>
+  </div>
+  <div class="join-find-wrap">
+    <div class="left">
+      <a href="/nlogin/findUsrId">아이디찾기</a>
+      <span class="bar"></span>
+      <a href="/nlogin/findUsrPwd">비밀번호찾기</a>
+    </div>
+    <div class="right">
+      <a href="/nlogin/usr?Mcode=10004">회원가입</a>
     </div>
   </div>
+</div>
+
+
+
+
+
+</div>
 </template>
 
 <script>
-import '@/assets/css/LoginCSS.css'
+  import '@/assets/css/LoginCSS.css'
 
-export default {
-  data() {
-    return {
-      images: [
-        {
-          src: "/images/userUploads/int/2024/09/MB_20240930_01.png",
-          alt: "정부24 100배 활용방법 인감증명서 발급 서비스",
-          link: "https://blog.naver.com/korea_gov/223597233237",
-        },
-        {
-          src: "/images/userUploads/int/2025/01/20250110_main.png",
-          alt: "2024년 귀속 연말정산 서류 한번에 발급 받으세요.",
-          link: "https://blog.naver.com/korea_gov/223721725554",
-        },
-      ],
-      keywords: ["주민등록등본", "가족관계증명서", "세목별과세증명서", "전입신고", "여권재발급"],
-      searchQuery: "",
-      currentSlide: 1,
-      totalSlides: 2,
-    };
-  },
-  methods: {
-    prevSlide() {
-      this.currentSlide =
-        this.currentSlide === 1 ? this.totalSlides : this.currentSlide - 1;
-    },
-    nextSlide() {
-      this.currentSlide =
-        this.currentSlide === this.totalSlides ? 1 : this.currentSlide + 1;
-    },
-    searchKeyword(keyword) {
-      this.searchQuery = keyword;
-    },
-    search() {
-      console.log("검색어:", this.searchQuery);
-    },
-  },
-};
+  export default{
+
+  };
 </script>
+
+<style>
+.join-find-wrap .right {
+    float: right;
+}
+
+.join-find-wrap span.bar {
+    display: inline-block;
+    margin: 0 8px;
+    width: 1px;
+    height: 10px;
+    background: #ddd;
+}
+
+.join-find-wrap a {
+    display: inline-block;
+    font-size: 17px;
+    line-height: 22px;
+    color: #1d1d1d;
+    letter-spacing: -0.34px;
+    font-weight: 400;
+}
+
+.join-find-wrap .left {
+    float: left;
+}
+
+.login-wrap .join-find-wrap {
+    overflow: hidden;
+    margin: 25px 0 0;
+}
+
+.login-wrap .login-info a.btn-login {
+    margin: 40px 0 0;
+    display: block;
+    width: 100%;
+    height: 52px;
+    line-height: 52px;
+    font-weight: bold;
+    font-size: 17px;
+    color: #fff;
+    background: #3382e9;
+    border-radius: 8px;
+}
+
+.userinfo-box {
+    width: 400px;
+    border: 1px solid #e2e5eb;
+    border-radius: 12px;
+    box-shadow: 10px 10px 27px rgba(0, 0, 0, 0.06);
+}
+
+.main-visual .inner-wrap {
+    display: flex
+;
+    justify-content: space-between;
+}
+.renew2022-main-wrap .inner-wrap {
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+}
+
+.login-wrap {
+    padding: 163px 26px 28px 26px;
+    text-align: center;
+    background: url(@/assets/images/ico-main-login.png) center top 60px no-repeat;
+}
+.renew2022-main-wrap *, .mygov-renew * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body, div, dl, dt, dd, th, td, ul, ol, li, h1, h2, h3, h4, h5, h6, form, fieldset, p, blockquote, button, input, textarea {
+    margin: 0;
+    padding: 0;
+}
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+* {
+    font-family: "Pretendard GOV", -apple-system, "Apple SD Gothic Neo", "Apple Gothic", BlinkMacSystemFont, sans-serif !important;
+}
+
+</style>
